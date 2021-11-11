6cc92491b332adff6e53596edd8a30d5ebcc1313
Signal Fish <signalfish38861721@gmail.com>
2021-11-11 00:16:38 -0500

    This project has been completed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-import "./styles/style.scss"
+import '../node_modules/@popperjs/core'
+import  "../node_modules/bootstrap/";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './styles/style.scss'
